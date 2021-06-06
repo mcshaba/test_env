78543e66b89acfa46de726a1e73023528663eaa7
Shaba Michael <shabaokare@gmail.com>
2021-06-06 20:51:32 +0100

    Final Commit

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -45,33 +45,44 @@ class _MyTabbedPageState extends State<MyTabbedPage>
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: TabBar(
-            controller: tabController,
-            tabs: myTabs,
-            indicatorWeight: 5,
-            indicatorColor: Colors.white,
-            labelColor: Colors.white,
+    return WillPopScope(
+      onWillPop: () async {
+        if(tabController.index != 0) {
+          setState(() {
+            tabController.index = 0;
+          });
+          return false;
+        }
+        return true;
+      },
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: TabBar(
+              controller: tabController,
+              tabs: myTabs,
+              indicatorWeight: 5,
+              indicatorColor: Colors.white,
+              labelColor: Colors.white,
 
+            ),
           ),
+          body:  TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            key: RIKeys.riKey1,
+            controller: tabController,
+            children: _pages.map((Widget tab) {
+              return tab;
+            }).toList(),
+          ),
+          // floatingActionButton: new FloatingActionButton(
+          //   onPressed: () =>
+          //       tabController.animateTo((tabController.index + 1) % 2),
+          //   // Switch tabs
+          //   child: new Icon(Icons.swap_horiz),
+          // ),
         ),
-        body:  TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          key: RIKeys.riKey1,
-          controller: tabController,
-          children: _pages.map((Widget tab) {
-            return tab;
-          }).toList(),
-        ),
-        // floatingActionButton: new FloatingActionButton(
-        //   onPressed: () =>
-        //       tabController.animateTo((tabController.index + 1) % 2),
-        //   // Switch tabs
-        //   child: new Icon(Icons.swap_horiz),
-        // ),
       ),
     );
   }
